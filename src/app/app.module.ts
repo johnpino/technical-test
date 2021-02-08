43aa3e95ae06909104ddebcaa125a1217682c128
John Pino <freelance@johnpino.me>
2021-02-07 19:25:27 -0500

    Add initial components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeroComponent } from './hero/hero.component';
+import { PrimaryNavigationComponent } from './shared/primary-navigation/primary-navigation.component';
+import { NotificationComponent } from './shared/notification/notification.component';
+import { VotingCardComponent } from './shared/voting-card/voting-card.component';
+import { HightlightPanelComponent } from './shared/hightlight-panel/hightlight-panel.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { MainContentComponent } from './main-content/main-content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroComponent,
+    PrimaryNavigationComponent,
+    NotificationComponent,
+    VotingCardComponent,
+    HightlightPanelComponent,
+    FooterComponent,
+    HeaderComponent,
+    MainContentComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/shared/hightlight-panel/hightlight-panel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hightlight-panel',
+  templateUrl: './hightlight-panel.component.html',
+  styleUrls: ['./hightlight-panel.component.scss']
+})
+export class HightlightPanelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
